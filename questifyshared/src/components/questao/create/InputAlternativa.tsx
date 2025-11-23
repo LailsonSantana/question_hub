12c23115d1f3ed6aff279bbc577c25ba9c34ce1e
Lailson Santana <lailsonbit@gmail.com>
2025-11-22 21:50:40 -0300

    Mudando o botão para o tipo radio

--- questifyshared/src/components/questao/create/InputAlternativa.tsx
+++ questifyshared/src/components/questao/create/InputAlternativa.tsx
@@ -28,7 +28,7 @@ const InputAlternativa: React.FC<InputAlternativaProps> = ({
           <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">Correta</label>
           <input
             id="default-checkbox"
-            type="checkbox"
+            type="radio"
             checked={isSelected}
             onChange={onSelect}
             className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
